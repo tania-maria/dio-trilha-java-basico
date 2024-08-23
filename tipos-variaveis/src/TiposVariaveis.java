31969f4d9f0b4ec3574f923ea610f00694691103
Tania <t.maria.dd2@gmail.com>
2024-08-23 11:45:54 -0300

    Variavel

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,27 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        /*
+         * O sistema pede para atualiza as informações e fica assim : isso recebe o nome
+         * de cast
+         */
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 10;
+
+        System.out.println(numero);
+        // declaração de variavel q não pode ser mudada sempre o nome deve estar em
+        // caixa alta e a palavra e final na frente como mostra a baixo:
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
+        /*
+         * estudar tipos primitivos
+         * estudar a classe que representa texto manipulação
+         * 
+         */
     }
 }
