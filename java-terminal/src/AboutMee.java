55a04e3c58feca353e0847ef8cd41796461dbf8d
Tania <t.maria.dd2@gmail.com>
2024-08-29 16:10:07 -0300

    meu projeto conta banco

---
+++ java-terminal/src/AboutMee.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMee {
+
+    public static void main(String[] args) {
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println(" Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println(" Digite sua idade");
+        int idade = scanner.nextInt();
+
+        // Imprimindo os dados obtidos pelo usuario1.70
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura e " + altura + "cm ");
+
+    }
+}
---
+++ java-terminal/src/Teste.java
@@ -0,0 +1,8 @@
+public class Teste {
+    public static void main(String[] args) throws Exception {
+        int numero1 = 1;
+        String numero2 = "2";
+        System.out.println(numero1 + numero2);
+    }
+
+}
